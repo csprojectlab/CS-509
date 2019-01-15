93e6fdeba124b3a59e9b9fff51380d4b97074576
csprojectlab <aridaman_nandan@outlook.com>
2019-01-15 18:40:29 +0530

    merge and quick sort

--- arrays/algo.js
+++ arrays/algo.js
@@ -2,8 +2,85 @@ let swap_count = 0;
 let start_time = 0;
 let end_time = 0;
 
+function quick_sort (data, attribute) {
+    quick_sort_util (data, attribute, 0, data.length - 1)
+    return {data: data}
+}
 
+function quick_sort_util (data, attribute, low, high) {
+    if(low < high) {
+        let partition = quick_sort_partition (data, attribute, low, high, high) // Choosing the last as pivot.
+        quick_sort_util (data, attribute, low, partition - 1)
+        quick_sort_util (data, attribute, partition + 1, high)
+    }
+}
 
+function quick_sort_partition (data, attribute, low, high, pivot) {
+    let index = low;
+    for (let i = low; i <= high; i++) {
+        if(compareTo (data[i], data[pivot], attribute) <= 0) {
+            swap (data, index, i);
+            index++;
+        }
+    }
+    swap (data, index, pivot)
+    return index;
+}
+
+/**
+ * 
+ * @param {*} data 
+ * @param {*} attribute 
+ */
+function merge_sort (data, attribute) {
+
+    merge_sort_util (data, attribute , 0, data.length - 1);
+    
+    return {data: data}
+}
+
+function merge_sort_util (data, attribute, low, high) {
+    if(low < high) {
+        let mid = Math.floor((low + high) / 2)
+        this.merge_sort_util(data, attribute, low, mid)
+        this.merge_sort_util (data, attribute, mid+1, high)
+        this.merge_util (data, attribute, low, mid, high)
+    }
+}
+
+function merge_util (data, attribute, low, mid, high) {
+    let result = [];
+    let left = low;
+    let right = mid + 1;
+    
+    while(left <= mid && right <= high) {
+        if(compareTo (data[left], data[right], attribute) <= 0) {
+            result.push (data[left])
+            left++;
+        } else {
+            result.push(data[right])
+            right++;
+        }
+    }
+    while (left <= mid) {
+        result.push(data[left])
+        left++;
+    }
+    while(right <= high) {
+        result.push(data[right])
+        right++
+    }
+    let index = 0;
+    for (let i = low; i <= high; i++) {
+        data[i] = result[index];
+        index++;
+    }
+}
+/**
+ * 
+ * @param {*} data 
+ * @param {*} attribute 
+ */
 function insertion_sort (data, attribute) {
     start_time = new Date().getTime();
     for (let i = 1; i < data.length; i++) {
